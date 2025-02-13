53af67dbeaf18aa3331dadd0cc883fa8c3064d03
jameelkhan4981 <35912328+jameelkhan4981@users.noreply.github.com>
2025-02-13 02:00:26 -0500

    Create s3.tf
    s3 terraform

---
+++ s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-private-bucket"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
