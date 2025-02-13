1dd830d9e1ee1f83897402476b3967547dc7419a
jameelkhan4981 <35912328+jameelkhan4981@users.noreply.github.com>
2025-02-13 02:43:20 -0500

    Update s3.tf
    replaced tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "my_bucket" {
+/*resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-private-bucket"
   acl    = "private"
 
@@ -10,13 +10,41 @@ resource "aws_s3_bucket" "my_bucket" {
       }
     }
   }
+}*/
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = mybucket.example.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+/*resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.my_bucket.id
 
   block_public_acls   = true
   block_public_policy = true
   ignore_public_acls  = true
   restrict_public_buckets = true
-}
+}*/
+
