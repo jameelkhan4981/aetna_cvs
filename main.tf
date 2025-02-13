fe5d4f2ba3b0f469622320eaf97baabb2592e8d2
jameelkhan4981 <35912328+jameelkhan4981@users.noreply.github.com>
2025-02-13 01:39:46 -0500

    Update main.tf
    updated image id

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami           = "ami-085ad6ae776d8f09c" # Amazon Linux 2
   instance_type = "t2.micro"
 
   tags = {
