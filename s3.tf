03b2ada80b6fb47d8f8921c5f975cc94e915c78b
jameelkhan4981 <35912328+jameelkhan4981@users.noreply.github.com>
2025-02-13 02:10:53 -0500

    Update s3.tf
    update tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-private-bucket"
   acl    = "private"
 
-  server_side_encryption_configuration {
+  aws_s3_bucket_server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
